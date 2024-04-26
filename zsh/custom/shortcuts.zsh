e47979c69d1fe2d61b8fc7c6ac1a21772584a172
Kuroi Kyu <kuroikyu@users.noreply.github.com>
2024-04-26 21:06:57 +0200

    fix: remove quotes othewise paths don't work?

--- zsh/custom/shortcuts.zsh
+++ zsh/custom/shortcuts.zsh
@@ -1,7 +1,6 @@
 # Projects
-advtoolsWin="/mnt/c/Users/kuroikyu/Projects/adventurers-tools/"
-advtools="~/Developer/AdventurersTools/adventurers-tools/"
-kuroi="~/Developer/kuroikyu/"
-krust="~/Developer/kuroikyu/rust/"
-koom="~/Developer/kuroikyu/out-of-mana/"
-
+advtoolsWin=/mnt/c/Users/kuroikyu/Projects/adventurers-tools/
+advtools=~/Developer/AdventurersTools/adventurers-tools/
+kuroi=~/Developer/kuroikyu/
+krust=~/Developer/kuroikyu/rust/
+koom=~/Developer/kuroikyu/out-of-mana/
