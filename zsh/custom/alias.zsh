c27fd7b026d4b78fd9df841fa9ae6076a0baafbf
Kuroi Kyu <hello@kuroikyu.com>
2024-11-30 17:31:59 +0100

    feat: add ProtonVPN port forwarding helper
    Creates UDP and TCP port mappings, then loops `natpmpc` so that it
    doesn't epire.
    
    Source:
    https://protonvpn.com/support/port-forwarding-manual-setup/#how-to-use-port-forwarding

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -8,3 +8,5 @@ alias gcnss!="git commit -v -s -S --no-edit --amend"
 # Reboot to Windows
 alias reboottowindows="sudo efibootmgr -n 1; sudo systemctl reboot"
 
+alias protonport="while true ; do date ; natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 || { echo -e 'ERROR with natpmpc command \a' ; break ; } ; sleep 45 ; done"
+
