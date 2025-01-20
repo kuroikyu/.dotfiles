8ce1f2a0ea8fb0dafd51ed946d0b6feabe139f2b
Kuroi Kyu <hello@kuroikyu.com>
2025-01-20 23:26:41 +0100

    refactor: comments and small update to alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,5 +1,5 @@
-# Example aliases
-alias zshconfig="nvim ~/.zshrc"
+# General
+alias zshconfig="nvim ~/.dotfiles/zsh/.zshrc"
 alias ohmyzsh="nvim ~/.oh-my-zsh"
 
 # Git
@@ -9,5 +9,6 @@ alias gcnss!="git commit -v -s -S --no-edit --amend"
 # Reboot to Windows
 alias reboottowindows="sudo efibootmgr -n 1; sudo systemctl reboot"
 
+# Proton VPN
 alias protonport="while true ; do date ; natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 || { echo -e 'ERROR with natpmpc command \a' ; break ; } ; sleep 45 ; done"
 
